2b8064410ced410d837ad710efaa115cb918bfae
Urian Viera <urian1213viera@example.com>
2023-04-08 16:25:56 -0500

    personalizando estructura

--- app/app.py
+++ app/app.py
@@ -7,9 +7,21 @@ app = Flask(__name__)
 application = app
 app.secret_key = '97110c78ae51a45af397be6534caef90ebb9b1dcb3380af008f90b23a5d1616bf19bc29098105da20fe'
 
+
+
+@app.route('/')
+def inicio():
+    return render_template('base_index.html')
+
+
+@app.route('/info', methods=['GET', 'POST'])
+def editProfile():
+    return render_template('public/pages/info.html')
+    #return redirect(url_for('inicio'))
+
+
+
 # Redireccionando cuando la página no existe
-
-
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html', error=error)
@@ -18,37 +30,6 @@ def not_found(error):
 @app.errorhandler(500)
 def server_error(error):
     return render_template('500.html', error=error)
-
-# Creando mi Decorador para el Home
-
-
-@app.route('/')
-def inicio():
-    return render_template('base_index.html')
-
-
-@app.route('/edit-profile', methods=['GET', 'POST'])
-def editProfile():
-    return redirect(url_for('inicio'))
-
-
-@app.route('/logout')
-def logout():
-    return 'salir'
-
-
-@app.route('/demo')
-def hello():
-    todos = ['Comprar cafe', 'Enviar solicitud de compra',
-             'Entregar video a productor ']
-    user_ip = request.cookies.get('user_ip')
-    context = {
-        'user_ip': user_ip,
-        'todos': todos,
-    }
-    return render_template('public/demo.html', **context)
-
-
 
 
 
