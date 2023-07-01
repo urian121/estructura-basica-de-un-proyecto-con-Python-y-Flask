c6114c1c32b7ee612f8ae65e72299b84da3b7e5a
UrianV <urian1213viera@gmail.com>
2023-06-30 22:55:05 -0500

    agregando algunos comentarios

--- app/app.py
+++ app/app.py
@@ -1,14 +1,20 @@
+# Importando la clase Flask de flask y algunos paquetes.
 from flask import Flask, request, redirect, render_template, url_for
 
 
-# Declarando nombre de la aplicación e inicializando, crear la aplicación Flask
+# Declarando nombre de la aplicación e inicializando,
+# crear la aplicación Flask
+# Creando una app instanciando la clase Flask
 app = Flask(__name__)
 application = app
 app.secret_key = '97110c78ae51a45af397be6534caef90ebb9b1dcb3380af008f90b23a5d1616bf19bc29098105da20fe'
 
 
+# Definiendo algunas rutas (parametros -argumentos - funciones)
 @app.route('/')
+# Definiendo una funcion llamada inicio
 def inicio():
+    # Retornando una vista sin parametros
     return render_template('base_index.html')
 
 
@@ -36,4 +42,5 @@ def server_error(error):
 
 # Ejecutando el objeto Flask
 if __name__ == '__main__':
+    # Método que indica la app, con la dirección, puerto y mode de argumento(debug)
     app.run(debug=True, port=5600)
